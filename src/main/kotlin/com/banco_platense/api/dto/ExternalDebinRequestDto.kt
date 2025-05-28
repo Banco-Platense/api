2ed7fb627dc9703d8231045f6503bc8b1ef13bf6
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2025-05-28 16:53:13 -0300

    fix

--- src/main/kotlin/com/banco_platense/api/dto/ExternalDebinRequestDto.kt
+++ src/main/kotlin/com/banco_platense/api/dto/ExternalDebinRequestDto.kt
@@ -3,7 +3,7 @@ package com.banco_platense.api.dto
 /**
  * Request DTO for external debit transactions.
  */
-data class ExternalDebitRequestDto(
+data class ExternalDebinRequestDto(
     val amount: Double,
     val description: String,
     val externalWalletInfo: String
