c2f7499377e659983dff6975c0812a1cec60febc
Felipe Germano <fgermano@mail.austral.edu.ar>
2025-06-04 16:40:58 -0300

    Debin external service

--- external-services-mock/src/main/kotlin/com/banco_platense/externalservicesmock/controller/ExternalMockController.kt
+++ external-services-mock/src/main/kotlin/com/banco_platense/externalservicesmock/controller/ExternalMockController.kt
@@ -14,6 +14,11 @@ import kotlin.random.Random
 @RequestMapping("/external")
 class ExternalMockController {
 
+    companion object {
+        private const val ACCEPT_ACCOUNT = "0001"
+        private const val REJECT_ACCOUNT = "0002"
+    }
+
     @PostMapping("/top-up")
     fun topUp(@RequestBody request: AccountRequestDto): ResponseEntity<ExternalResponseDto> {
         return if (Random.nextDouble() <= 0.8) {
@@ -26,11 +31,12 @@ class ExternalMockController {
 
     @PostMapping("/debin/request")
     fun debinRequest(@RequestBody request: AccountRequestDto): ResponseEntity<ExternalResponseDto> {
-        return if (Random.nextDouble() <= 0.8) {
-            ResponseEntity.ok(ExternalResponseDto("success", "Money loaded successfully"))
-        } else {
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ExternalResponseDto("error", "External service failed"))
+        return when (request.accountNumber) {
+            ACCEPT_ACCOUNT -> ResponseEntity.ok(ExternalResponseDto("success", "Money loaded successfully"))
+            REJECT_ACCOUNT -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ExternalResponseDto("error", "Error, no soy hincha fanatico de platense asi que no acepto el DEBIN"))
+            else -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ExternalResponseDto("error", "Account not found"))
         }
     }
 } 
